662439aa997490c255de0355e93a38dbc9caa228
IMoisasZ <mopri08@gmail.com>
2021-08-15 01:39:53 -0300

    Organizando o coigo. Deixando na pasta route apenas a chamada para as requisicoes, passando as validacoes para os contollers, as regras de negocios para o service e as leituras e gravacoes no banco par o repository

---
+++ routes/account.route.js
@@ -0,0 +1,24 @@
+import express from "express";
+import AccounController from '../controllers/account.controller.js';
+
+const router = express.Router();
+
+router.post("/", AccounController.createAccount)
+
+router.get("/", AccounController.getAccounts)
+
+router.get("/:id", AccounController.getAccount)
+
+router.delete("/:id", AccounController.deleteAccount)
+
+router.put('/', AccounController.updateAccount)
+
+router.patch('/updateBalance', AccounController.updateBalance)
+
+router.use((err, req, res, next) =>{
+    global.logger.error(`${req.method} ${req.baseUrl} - ${err.message}`)
+    console.log(err);
+    res.status(400).send({ error: err.message });
+})
+
+export default router;
